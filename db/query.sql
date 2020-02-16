031d75bbe12dacf14226232dc4d02a30f82f42c2
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2020-02-16 18:20:59 +0200

    add some good queries

--- db/query.sql
+++ db/query.sql
@@ -1,1 +1,6 @@
 SELECT tasks.id, tasks.summary, tasks.description, assignee.id, assignee.name FROM tasks INNER JOIN workers as assignee ON assignee.id = tasks.assignee_id
+
+DELETE FROM workers;
+DELETE FROM tasks;
+
+SELECT * FROM workers;
