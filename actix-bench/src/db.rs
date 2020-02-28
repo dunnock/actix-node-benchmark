ab5ae3240807c2afaafd2a7f4e041f6d74b77052
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2020-02-28 13:16:37 +0200

    fixed compile errors

--- actix-bench/src/db.rs
+++ actix-bench/src/db.rs
@@ -2,7 +2,7 @@ use crate::{errors::BenchError, models::Task, GetTasksQuery};
 use deadpool_postgres::Pool;
 use tokio_pg_mapper::FromTokioPostgresRow;
 use tokio_postgres::types::Type;
-use std::convert::TryFrom;
+use std::sync::Arc;
 
 impl GetTasksQuery {
 	pub fn is_full(&self) -> bool {
@@ -10,13 +10,13 @@ impl GetTasksQuery {
 	}
 }
 
-pub async fn get_tasks(pool: Pool, query: GetTasksQuery) -> Result<Vec<Task>, BenchError> {
+pub async fn get_tasks(pool: Arc<Pool>, query: GetTasksQuery) -> Result<Vec<Task>, BenchError> {
 	let _stmt = if query.is_full() { 
 		include_str!("sql/get_tasks_full.sql")
 	} else {
 		include_str!("sql/get_tasks.sql")
 	};
-    let client = db_pool.get().await?;
+    let client = pool.get().await?;
 	let stmt = client.prepare_typed(&_stmt, &[Type::VARCHAR, Type::VARCHAR, Type::OID]).await.unwrap();
 	let like = |s: Option<String>| s.map(|s| format!("%{}%", s));
 
@@ -26,7 +26,7 @@ pub async fn get_tasks(pool: Pool, query: GetTasksQuery) -> Result<Vec<Task>, Be
 			&[
 				&like(query.assignee_name),
 				&like(query.summary),
-				&query.limit.unwrap_or(10)
+				&query.limit.or(Some(10))
 			],
 		)
 		.await?
