4baf8c5323aad34a8276cf9ae35eff6300f3484d
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2020-02-29 00:17:05 +0200

    make fast recycling manager default option

--- actix-bench/src/config.rs
+++ actix-bench/src/config.rs
@@ -1,5 +1,6 @@
 pub use ::config::ConfigError;
 use serde::Deserialize;
+use deadpool_postgres::{RecyclingMethod, ManagerConfig};
 
 #[derive(Deserialize)]
 pub struct Config {
@@ -11,6 +12,8 @@ impl Config {
 	pub fn from_env() -> Result<Self, ConfigError> {
 		let mut cfg = ::config::Config::new();
 		cfg.merge(::config::Environment::new())?;
-		cfg.try_into()
+		let mut cfg: Config = cfg.try_into()?;
+		cfg.pg.manager = Some(ManagerConfig { recycling_method: RecyclingMethod::Fast });
+		Ok(cfg)
 	}
 }
