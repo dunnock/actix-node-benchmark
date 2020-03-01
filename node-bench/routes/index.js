cc6b0952e90fd5fbc76a0b2a25e7608f30852ced
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2020-03-01 13:48:59 +0200

    add limit parameter error handler

--- node-bench/routes/index.js
+++ node-bench/routes/index.js
@@ -5,9 +5,15 @@ const router = express.Router()
 const db = require('../db/knex.js');
 const { createError } = require('./error.js');
 
-router.get('/tasks', (req, res) => {
+router.get('/tasks', (req, res, next) => {
     let { assignee_name, summary, limit, full } = req.query;
-    full = full == "true"; 
+    full = full == "true";
+    if (!!limit && isNaN(limit)) {
+        return next(createError({
+            status: 400, 
+            message: "limit query parameter should be a number"
+        }));
+    }
 
     db.get_tasks(assignee_name, summary, limit, full)
         .then(tasks => res.send(tasks))
