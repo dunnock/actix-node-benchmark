3681183dd0b20403fe32be41542ddcc19027cf3f
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2020-03-01 13:20:42 +0200

    ident to spaces

--- actix-bench/src/db.rs
+++ actix-bench/src/db.rs
@@ -5,30 +5,30 @@ use tokio_postgres::types::Type;
 use std::sync::Arc;
 
 impl GetTasksQuery {
-	pub fn get_statement(&self) -> &'static str {
-		if self.full.is_some() && self.full.unwrap() { 
-			include_str!("sql/get_tasks_full.sql")
-		} else {
-			include_str!("sql/get_tasks.sql")
-		}
-	}
+    pub fn get_statement(&self) -> &'static str {
+        if self.full.is_some() && self.full.unwrap() { 
+            include_str!("sql/get_tasks_full.sql")
+        } else {
+            include_str!("sql/get_tasks.sql")
+        }
+    }
 }
 
 fn like(s: Option<String>) -> Option<String> {
-	s.map(|s| format!("%{}%", s))
+    s.map(|s| format!("%{}%", s))
 }
 
 pub async fn get_tasks(pool: Arc<Pool>, query: GetTasksQuery) -> Result<Vec<Task>, BenchError> {
-	let _stmt = query.get_statement();
+    let _stmt = query.get_statement();
 
     let client = pool.get().await?;
-	let stmt = client.prepare_typed(&_stmt, &[Type::VARCHAR, Type::VARCHAR, Type::OID]).await.unwrap();
+    let stmt = client.prepare_typed(&_stmt, &[Type::VARCHAR, Type::VARCHAR, Type::OID]).await.unwrap();
 
-	client.query(
-			&stmt,
-			&[ &like(query.assignee_name), &like(query.summary), &query.limit.or(Some(10)) ],
-		).await?
-		.iter()
-		.map(|row| Task::from_row_ref(row).map_err(BenchError::from))
-		.collect()
+    client.query(
+            &stmt,
+            &[ &like(query.assignee_name), &like(query.summary), &query.limit.or(Some(10)) ],
+        ).await?
+        .iter()
+        .map(|row| Task::from_row_ref(row).map_err(BenchError::from))
+        .collect()
 }
